c13a3eebbcbe4de8a7c0e1306d98d30238f5d8aa
leniangraeni <alifhilmirc@gmail.com>
2018-07-04 18:03:40 +0700

    read posts for mahasiswa

--- src/pages/home-mhs/home-mhs.ts
+++ src/pages/home-mhs/home-mhs.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, MenuController, ToastController, AlertController } from 'ionic-angular';
 import { ProfilePage } from '../profile/profile';
 import { DetailPage } from '../detail/detail';
 import { ListPage } from '../list/list';
+import { Http } from '@angular/http';
+import { Data } from '../../provider/data';
+import { Menu } from 'ionic-angular/components/app/menu-interface';
 
 @IonicPage()
 @Component({
@@ -10,24 +13,55 @@ import { ListPage } from '../list/list';
   templateUrl: 'home-mhs.html',
 })
 export class HomeMhsPage {
- 
-  searchQuery: string = '';
+  
+  posts: Array<{}>;
   items: string[];
+  response: any;
 
   constructor(
+    public menuCtrl: MenuController,
     public navCtrl: NavController, 
-    public navParams: NavParams,
-    public menuCtrl: MenuController
+    public navParams: NavParams, 
+    public http: Http,
+    public toastCtrl: ToastController, 
+    public alertCtrl: AlertController, 
+    public DataStorage: Data
   ) {
-  }
+    let linkgetposts = 'http://localhost/rest_api/post.php';
+  	this.posts = [];
+  	this.response = {};
 
-  initializeItems() {
-    this.items = [
-      'Tanoto',
-      'PPA'
-    ];
-  }
+  	this.http.get(linkgetposts).subscribe(data => {
+  		this.response = data.json();
+  		if(this.response.status == "200"){
+          console.log(this.response.data);
+          for (var i=0;i<this.response.data.length;i++) {
+          	  var thedatetime = new Date(+this.response.data[i].waktu);
+          	  var options = { weekday: 'long', year: 'numeric',
+          	  month: 'short', day: 'numeric' }
 
+          	  let posts_elem =
+          	  {id_post: this.response.data[i].id_post, userid_prv: this.response.data[i].userid_prv, 
+              nama_prv: this.response.data[i].nama_prv, judul: this.response.data[i].judul, 
+              deskripsi: this.response.data[i].deskripsi, waktu: thedatetime.toLocaleDateString('id-ID', options)};
+          	  this.posts.push(posts_elem);
+	        }
+        } else {
+          let conf = this.alertCtrl.create({
+          	title: 'Tidak ada post',
+            message: 'Belum ada post saat ini. Silakan coba lagi nanti',
+            buttons: [
+			{
+		  		text: 'OK',
+		  		handler: () => {
+		    	console.log('OK clicked');
+		  		}
+			}]
+          });
+          conf.present();
+        }
+  	});
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomeMhsPage');
@@ -48,19 +82,4 @@ export class HomeMhsPage {
   onLoadList(){
     this.navCtrl.push(ListPage);
   }
-
-  getItems(ev: any) {
-    this.initializeItems();
-
-    // set val to the value of the searchbar
-    let val = ev.target.value;
-
-    // if the value is an empty string don't filter the items
-    if (val && val.trim() != '') {
-      this.items = this.items.filter((item) => {
-        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      })
-    }
-  }
-
 }
